0f86ba6cb673fc330f216498bc49614465ec3566
RyeAndhra <raiandhra_abdillah_30rpl@student.smktelkom-mlg.sch.id>
2023-03-10 12:58:10 +0700

    Change Format

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -17,7 +17,7 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="card card-primary card-outline">
+                            <div class="card card-dark card-outline">
                                 <div class="card-body">
                                     <h5 class="card-title">Welcome!</h5>
 
